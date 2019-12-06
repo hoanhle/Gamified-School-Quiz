7b2e43df17757bd759cc5eb76f8a13fe9b349551
Juhani Säteri <juhani.sateri@tuni.fi>
2019-12-06 16:14:42 +0200

    Management view is only visible to andmins

--- routes/managementView.js
+++ routes/managementView.js
@@ -3,8 +3,9 @@
 const express = require('express');
 const router = express.Router();
 const mngController = require('../controllers/managementView');
+const auth = require('../middleware/auth')
 
-router.get('/', mngController.showManagementView);
+router.get('/', auth.ensureAdmin, mngController.showManagementView);
 // no posts
 //router.post('/', mngController.gradeExercise);
 
