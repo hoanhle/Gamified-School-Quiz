7975f8ea9dd084d80a44dc9a333f5db717def50d
Hoanh Le <hoanh.le@tuni.fi>
2019-12-17 16:11:38 +0200

    Generate new random question and 4 answer on the go

--- controllers/game.js
+++ controllers/game.js
@@ -1,46 +1,71 @@
 'use strict';
 
 const Questionnaire = require('../models/questionnaire');
+const db = require('../controllers/db');
 
 module.exports = {
+	/**
+     * Returns start menu game
+     * @param {Object} request is express request object
+     * @param {Object} response is express response object
+     */
 	async showStartGame(request, response) {
 		response.render('game');
 	},
 
+	/**
+     * Returns how to play menu
+     * @param {Object} request is express request object
+     * @param {Object} response is express response object
+     */
 	async showRules(request, response) {
 		response.render('rules');
 	},
 
+	/**
+     * Generate a random question from the questionaire
+     */
 	async generateRandomQuestion() {
-		const numQuestions = await Questionnaire.countDocuments({});
+		const questionaire = await Questionnaire.findOne({ title: 'Count without a calculator' });
+		const numQuestions = questionaire.questions.length;
 		const randomNumber = Math.floor(Math.random() * numQuestions);
-		return await Questionnaire.find().limit(1).skip(randomNumber);
+		return questionaire.questions[randomNumber];
+	},
+
+	/**
+     * Generate four option contains 1 true and 3 false
+     * @param {Object} question is question object contains title, options, maximum points
+     */
+	async generateOptions(question) {
+		const options = [ ...question.options ];
+		const chooseOptions = [];
+		let falseOptionNum = 0;
+		let trueOptionNum = 0;
+		while (falseOptionNum < 3 || trueOptionNum < 1) {
+			const randomNumber = Math.floor(Math.random() * options.length);
+			const randomOption = options[randomNumber];
+
+			if (randomOption.correctness && trueOptionNum < 1) {
+				chooseOptions.push(randomOption);
+				trueOptionNum += 1;
+			}
+
+			if (!randomOption.correctness && falseOptionNum < 3) {
+				chooseOptions.push(randomOption);
+				falseOptionNum += 1;
+			}
+		}
+
+		return chooseOptions;
 	},
 
 	async startGame(request, response) {
-		const randomQuestion = this.generateRandomQuestion;
-		// response.render('gameView', { randomQuestion });
-
+		const randomQuestion = await module.exports.generateRandomQuestion();
+		const title = randomQuestion.title;
+		const options = await module.exports.generateOptions(randomQuestion);
 		response.render('gameView', {
-			title: 'Select the calculations that result in 40',
-			options: [
-				{
-					option: '25 + 15',
-					correctness: true
-				},
-				{
-					option: '10 + 25',
-					correctness: false
-				},
-				{
-					option: '66 - 26',
-					correctness: false
-				},
-				{
-					option: '27 + 23',
-					correctness: false
-				}
-			]
+			title: title,
+			options: options
 		});
 	},
 
@@ -49,8 +74,13 @@ module.exports = {
 		console.log(typeof isCorrect);
 		// if correct, render the next random questions
 		if (isCorrect === 'true') {
-			const randomQuestion = this.generateRandomQuestion;
-			response.render('gameView', { randomQuestion });
+			const randomQuestion = await module.exports.generateRandomQuestion();
+			const title = randomQuestion.title;
+			const options = await module.exports.generateOptions(randomQuestion);
+			response.render('gameView', {
+				title: title,
+				options: options
+			});
 		} else {
 			const points = 2;
 			response.render('endGame', { points });
