4b1983296b1f4ba2238ba076acf59e7164fe5020
Hoanh Le <hoanh.le@tuni.fi>
2019-12-19 00:27:14 +0200

    Move functions from game controller to game model

--- controllers/game.js
+++ controllers/game.js
@@ -2,6 +2,7 @@
 
 const Questionnaire = require('../models/questionnaire');
 const db = require('../controllers/db');
+const Game = require('../models/game');
 
 module.exports = {
 	/**
@@ -23,46 +24,14 @@ module.exports = {
 	},
 
 	/**
-     * Generate a random question from the questionaire
+     * Start the game menu
+     * @param {Object} request is express request object
+     * @param {Object} response is express response object
      */
-	async generateRandomQuestion() {
-		const questionaire = await Questionnaire.findOne({ title: 'Count without a calculator' });
-		const numQuestions = questionaire.questions.length;
-		const randomNumber = Math.floor(Math.random() * numQuestions);
-		return questionaire.questions[randomNumber];
-	},
-
-	/**
-     * Generate four option contains 1 true and 3 false
-     * @param {Object} question is question object contains title, options, maximum points
-     */
-	async generateOptions(question) {
-		const options = [ ...question.options ];
-		const chooseOptions = [];
-		let falseOptionNum = 0;
-		let trueOptionNum = 0;
-		while (falseOptionNum < 3 || trueOptionNum < 1) {
-			const randomNumber = Math.floor(Math.random() * options.length);
-			const randomOption = options[randomNumber];
-
-			if (randomOption.correctness && trueOptionNum < 1) {
-				chooseOptions.push(randomOption);
-				trueOptionNum += 1;
-			}
-
-			if (!randomOption.correctness && falseOptionNum < 3) {
-				chooseOptions.push(randomOption);
-				falseOptionNum += 1;
-			}
-		}
-
-		return chooseOptions;
-	},
-
 	async startGame(request, response) {
-		const randomQuestion = await module.exports.generateRandomQuestion();
+		const randomQuestion = await Game.generateRandomQuestion();
 		const title = randomQuestion.title;
-		const options = await module.exports.generateOptions(randomQuestion);
+		const options = await Game.generateOptions(randomQuestion);
 		response.render('gameView', {
 			title: title,
 			options: options
@@ -71,16 +40,17 @@ module.exports = {
 
 	async gradeAnswer(request, response) {
 		const isCorrect = request.body.option;
-		console.log(typeof isCorrect);
 		// if correct, render the next random questions
 		if (isCorrect === 'true') {
-			const randomQuestion = await module.exports.generateRandomQuestion();
+			const randomQuestion = await Game.generateRandomQuestion();
 			const title = randomQuestion.title;
-			const options = await module.exports.generateOptions(randomQuestion);
+			const options = await Game.generateOptions(randomQuestion);
 			response.render('gameView', {
 				title: title,
 				options: options
 			});
+
+			console.log(request.cookies.point);
 		} else {
 			const points = 2;
 			response.render('endGame', { points });
---
+++ models/game.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const Questionnaire = require('../models/questionnaire');
+const db = require('../controllers/db');
+
+module.exports = {
+	/**
+     * Generate a random question from the questionaire
+     */
+	async generateRandomQuestion() {
+		const questionaire = await Questionnaire.findOne({
+			title: 'Count without a calculator'
+		});
+		const numQuestions = questionaire.questions.length;
+		const randomNumber = Math.floor(Math.random() * numQuestions);
+		return questionaire.questions[randomNumber];
+	},
+
+	/**
+     * Generate four option contains 1 true and 3 false
+     * @param {Object} question is question object contains title, options, maximum points
+     */
+	async generateOptions(question) {
+		const options = [ ...question.options ];
+		const chooseOptions = [];
+		let falseOptionNum = 0;
+		let trueOptionNum = 0;
+		while (falseOptionNum < 3 || trueOptionNum < 1) {
+			const randomNumber = Math.floor(Math.random() * options.length);
+			const randomOption = options[randomNumber];
+
+			if (randomOption.correctness && trueOptionNum < 1) {
+				chooseOptions.push(randomOption);
+				trueOptionNum += 1;
+			}
+
+			if (!randomOption.correctness && falseOptionNum < 3) {
+				chooseOptions.push(randomOption);
+				falseOptionNum += 1;
+			}
+		}
+
+		return chooseOptions;
+	}
+};
